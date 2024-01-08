f9185c21663b413f73075b6f27e4514c13c29ac0
Carol Acharya <carolacharya@Carols-MacBook-Air.local>
2024-01-08 14:36:45 +0545

    uuid implemented

--- quiz/admin.py
+++ quiz/admin.py
@@ -3,6 +3,13 @@ from .models import *
 
 # Register your models here.
 
-admin.site.register(Question)
 admin.site.register(Category)
+
+class AnswerAdmin(admin.StackedInline):
+    model=Answer
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines=[AnswerAdmin]
+
+admin.site.register(Question,QuestionAdmin)
 admin.site.register(Answer)
--- quiz/models.py
+++ quiz/models.py
@@ -13,13 +13,22 @@ class BaseModel(models.Model):
 
 class Category(BaseModel):
     category_name=models.CharField(max_length=100)
+    def __str__(self) -> str:
+        return self.category_name
+
 
 class Question(BaseModel):
     category=models.ForeignKey(Category,related_name='category', on_delete=models.CASCADE) #relatedname is to reverse the foreignkey/'reverserelationship' 
     question=models.CharField(max_length=100)
     marks=models.IntegerField(default=5)
+    def __str__(self) -> str:
+        return self.question
+
 
 class Answer(BaseModel):
     question=models.ForeignKey(Question,on_delete=models.CASCADE)
     answer=models.CharField(max_length=100)
     is_correct=models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.answer
